5981df7fcda1f04ecaf10efca4cd29b53f0d4f05
T5uku5hi <misaki.makino@gmail.com>
2020-04-30 08:07:17 +0900

    ✨ Add tokenizer

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ assert() {
 	./tmp
 	actual="$?"
 	
-	if [ "$actual" = "$expected" ]; then
+	if [[ "$actual" = "$expected" ]]; then
 		echo "$input => $actual"
 	else
 		echo "$input => $expected expected, but got $actual"
@@ -19,6 +19,7 @@ assert() {
 assert 0 0
 assert 42 42
 assert 21 "5+20-4"
+assert 41 " 12 + 34 - 5 "
 
 echo OK
 
